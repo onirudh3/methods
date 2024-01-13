4d930edec25c6e9061b971309421590e5f4ba123
Anirudh Ravishankar <onirudh3@gmail.com>
2024-01-13 18:07:05 +0100

    updates january 13

---
+++ script.R
@@ -0,0 +1,8 @@
+
+# Libraries and data ------------------------------------------------------
+
+library(tidyverse)
+
+# Data
+df <- read.csv("https://query.data.world/s/yocf2k2ox7qppp536knpbxfpdzjtrj?dws=00000",
+               header = T, stringsAsFactors = F)
