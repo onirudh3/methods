82054138b15eb21577b12605f40f8c65146fc836
Anirudh Ravishankar <onirudh3@gmail.com>
2024-01-14 18:20:09 +0100

    updates january 14

--- script.R
+++ script.R
@@ -6,3 +6,8 @@ library(tidyverse)
 # Data
 df <- read.csv("https://query.data.world/s/yocf2k2ox7qppp536knpbxfpdzjtrj?dws=00000",
                header = T, stringsAsFactors = F)
+
+
+# Smoking -----------------------------------------------------------------
+
+
